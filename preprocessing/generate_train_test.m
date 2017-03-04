018a3c25a6f4214469af1db1c55d584089bb6dfe
Šimon Pavlík <SimonPavlik@users.noreply.github.com>
2017-03-04 14:53:17 +0100

    process data

--- preprocessing/generate_train_test.m
+++ preprocessing/generate_train_test.m
@@ -1,57 +1,56 @@
+train_save_path = '。。。。。/data/cifar_10/train';
+test_save_path = '。。。。。/data/cifar_10/test';
 
-train_save_path = '/home/bohan/Documents/code/cvpr/data/cifar_10/train';
-test_save_path = '/home/bohan/Documents/code/cvpr/data/cifar_10/test';
+train_img_path = '。。。。。/data/cifar_10/images/train';
+test_img_path = '。。。。。/data/cifar_10/images/test/test_batch.mat';
 
-train_img_path = '/home/bohan/Documents/data/cifar_10/images/train';
-test_img_path = '/home/bohan/Documents/data/cifar_10/images/test';
+%% process train data
+last_img = 0;
+for batch = 1:5
+    load(strcat(train_img_path,'/data_batch_', num2str(batch), '.mat'));
+    data = reshape(data, [10000, 32, 32, 3]);
+    data = permute(data, [1, 3, 2, 4]);
+    
+    for e_idx = 1:10
 
-% prepare the labels by yourself
-load train_labels;
-load test_labels;
+        relevant_idxes = find(labels==(e_idx-1));
 
+        for k = 1:length(relevant_idxes)
 
- for e_idx = 1:10
-      
-     relevant_idxes = find(train_labels==(e_idx-1));
-        
-     for k = 1:length(relevant_idxes)
-         
-        save_img_path = fullfile(train_save_path, num2str(e_idx));
- 
+            save_img_path = fullfile(train_save_path, num2str(e_idx));
+
+            if ~isdir(save_img_path)
+                mkdir(save_img_path);
+            end
+            img_path = fullfile(save_img_path, sprintf('%04d.JPEG', last_img + relevant_idxes(k)));
+            
+            img = squeeze(data(relevant_idxes(k), :, :, :));
+            img = imresize(img, [224, 224]);   
+            imwrite(img, img_path, 'jpg', 'Quality', 100);
+        end 
+        last_img = k;
+    end
+end
+
+%% process test data
+load(test_img_path);
+data = reshape(data, [10000, 32, 32, 3]);
+data = permute(data, [1, 3, 2, 4]);
+for e_idx = 1:10
+
+    relevant_idxes = find(labels==(e_idx-1));
+
+    for k = 1:length(relevant_idxes)
+
+        save_img_path = fullfile(test_save_path, num2str(e_idx));
+
         if ~isdir(save_img_path)
             mkdir(save_img_path);
         end
         img_path = fullfile(save_img_path, sprintf('%04d.JPEG', relevant_idxes(k)));
-        raw_img_path = fullfile(train_img_path, sprintf('%04d.JPEG', relevant_idxes(k)));
-       img = im2uint8(imread(raw_img_path));
-       img = imresize(img, [224, 224]);   
-       imwrite(img, img_path, 'jpg', 'Quality', 100);
-        
-         
-     end 
-        
-     
- end
 
-
-for e_idx = 1:10
-    
-    relevant_idxes =find(test_labels == (e_idx-1));
-    
-
-    for j = 1:length(relevant_idxes)
-        
-       save_img_path = fullfile(test_save_path, num2str(e_idx));
-
-       if ~isdir(save_img_path)
-           mkdir(save_img_path);
-       end
-       img_path = fullfile(save_img_path, sprintf('%04d.JPEG', relevant_idxes(j)));
-       raw_img_path = fullfile(test_img_path, sprintf('%04d.JPEG', relevant_idxes(j)));
-       img = im2uint8(imread(raw_img_path));
-       img = imresize(img, [224, 224]);   
-       imwrite(img, img_path, 'jpg', 'Quality', 100);
-       
-        
+        img = squeeze(data(relevant_idxes(k), :, :, :));
+        img = imresize(img, [224, 224]);   
+        imwrite(img, img_path, 'jpg', 'Quality', 100);
     end
 end
